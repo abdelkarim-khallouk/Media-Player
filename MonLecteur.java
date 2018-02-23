6dab717229f9eb3e7a441e00d35b5126c2c52ef9
Abdelkarim KHALLOUK <khlgeek@gmail.com>
2018-02-23 22:25:21 +0000

    Add files via upload
    second commit

--- MonLecteur.java
+++ MonLecteur.java
@@ -1,19 +1,34 @@
    import javafx.application.Application;
    import javafx.scene.Group;
-   import javafx.scene.Scene;   
+   import javafx.scene.Scene;
    import javafx.stage.Stage;
-   import javafx.scene.shape.Circle;
+   import javafx.scene.media.Media;
+   import javafx.scene.media.MediaPlayer;
+   import javafx.scene.media.MediaView;  
 
    public class MonLecteur extends Application {         
+   //Specifier le fichier source Media
+      private static final String nomFichier = "file:/c:/Chanson.flv";// "http://download.oracle.com/otndocs/products/javafx/oow2010-2.flv";
    
    //Modifier la methode start
       public void start(Stage st) {
-         //Circle circ = new Circle(40, 40, 30);
-         //Group racine = new Group(circ);   
-         Group racine = new Group();         
-         Scene scene = new Scene(racine, 800, 500);
-         st.setTitle("MyPlayer");      
-         st.setScene(scene);              
+         Group racine = new Group();      
+         Scene scene = new Scene(racine, 544, 224);//800, 500
+         st.setTitle("FsrLecteur");      
+         st.setScene(scene);
+      
+      
+      //Creation du media
+         Media media = new Media(nomFichier);
+      
+      //Creation du media player
+         MediaPlayer mediaPlayer = new MediaPlayer(media);        
+         mediaPlayer.setAutoPlay(true); 
+      
+      //Creation mediaView         
+         MediaView mediaView = new MediaView(mediaPlayer); 
+         ((Group) scene.getRoot()).getChildren().add(mediaView);
+      
          st.show();     
       }        
    
