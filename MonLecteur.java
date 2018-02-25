a8abf1575d6ded9c8ea776c4412357c1083890dc
Abdelkarim KHALLOUK <khlgeek@gmail.com>
2018-02-25 11:38:33 +0000

    Update MonLecteur.java
    udate

--- MonLecteur.java
+++ MonLecteur.java
@@ -4,31 +4,33 @@
    import javafx.stage.Stage;
    import javafx.scene.media.Media;
    import javafx.scene.media.MediaPlayer;
-   import javafx.scene.media.MediaView;  
+   //import javafx.scene.media.MediaView;  
+   import javafx.util.Duration;
 
    public class MonLecteur extends Application {         
    //Specifier le fichier source Media
-      private static final String nomFichier = "file:/c:/Chanson.flv";// "http://download.oracle.com/otndocs/products/javafx/oow2010-2.flv";
+      private static final String MEDIA_URL = "file:/c:/Chanson.flv";// "http://download.oracle.com/otndocs/products/javafx/oow2010-2.flv";
    
    //Modifier la methode start
       public void start(Stage st) {
          Group racine = new Group();      
-         Scene scene = new Scene(racine, 544, 224);//800, 500
+         Scene scene = new Scene(racine, 800, 500);//544, 224
          st.setTitle("FsrLecteur");      
          st.setScene(scene);
-      
-      
-      //Creation du media
-         Media media = new Media(nomFichier);
+  
       
       //Creation du media player
+         Media media = new Media(MEDIA_URL);
          MediaPlayer mediaPlayer = new MediaPlayer(media);        
          mediaPlayer.setAutoPlay(true); 
+         mediaPlayer.setCycleCount(2); 									// * * * * * * * * * *
+         mediaPlayer.setStartTime(Duration.seconds(10));				// * * * * * * * * * *
+         mediaPlayer.setStopTime(Duration.seconds(20));				// * * * * * * * * * *
+      
       
       //Creation mediaView         
-         MediaView mediaView = new MediaView(mediaPlayer); 
-         ((Group) scene.getRoot()).getChildren().add(mediaView);
-      
+         MediaControl mediaControl = new MediaControl(mediaPlayer);
+         scene.setRoot(mediaControl);
          st.show();     
       }        
    
